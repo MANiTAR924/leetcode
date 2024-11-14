bf263ad70909533d786b9de77014733958076a2d
manitar <mac21077919050@gmail.com>
2024-11-14 06:29:07 +0200

    save

--- integerToRoman.js
+++ integerToRoman.js
@@ -1,33 +1,51 @@
 const nums ={
     1: 'I',
-    4: 'IV',
     5: 'V',
-    9: 'IX',
     10: 'X',
-    40: 'XL',
     50: 'L',
-    90: 'XC',
     100: 'C',
-    400: 'CD',
     500: 'D',
-    900: 'CM',
     1000: 'M',
 };
+// const numsi ={
+//     1: 'I',
+//     4: 'IV',
+//     5: 'V',
+//     9: 'IX',
+//     10: 'X',
+//     40: 'XL',
+//     50: 'L',
+//     90: 'XC',
+//     100: 'C',
+//     400: 'CD',
+//     500: 'D',
+//     900: 'CM',
+//     1000: 'M',
+// };
 
 function intToRoman(num){
    const arr = []; 
    const array = [];
-   let res = 0;
+   const arr0 = [];
     i = 0;
     while(i < num.toString().length){
-        arr.push(num.toString().charAt(i));
         array.push(nums[num.toString().charAt(i)]);
+        arr.push(num.toString().charAt(i))
     i++
     };        
-//    arr.reverse();
-    console.log(array.join(''));
-    const sun = arr.reduce((accumulator, currentValue) => accumulator + currentValue);
-    console.log(sun);
-    return sun; 
+    i0 = 0;
+    let n = ''
+    while(arr.length !== 0){
+        let len = arr.length;
+       
+        arr.shift()
+    i0++
+    }
+    array.reverse();
+    
+    console.log(array.join(' '));
+    console.log(arr.join(' '))
+    console.log(num);
+    console.log(arr0)
 };
 intToRoman(1994);
