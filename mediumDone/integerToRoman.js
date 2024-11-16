618426bcfed6dd9550be356cfa4ae71db4283f63
manitar <mac21077919050@gmail.com>
2024-11-16 20:10:56 +0200

    save

--- mediumDone/integerToRoman.js
+++ mediumDone/integerToRoman.js
@@ -7,26 +7,42 @@ const nums ={
     500: 'D',
     1000: 'M',
 };
-// const numsi ={
-//     1: 'I',
-//     4: 'IV',
-//     5: 'V',
-//     9: 'IX',
-//     10: 'X',
-//     40: 'XL',
-//     50: 'L',
-//     90: 'XC',
-//     100: 'C',
-//     400: 'CD',
-//     500: 'D',
-//     900: 'CM',
-//     1000: 'M',
-// };
+const numsi ={
+    1: 'I',
+    2: 'II',
+    3: 'III',
+    4: 'IV',
+    5: 'V',
+    6: 'VI',
+    7: 'VII',
+    8: 'VIII',
+    9: 'IX',
+    10: 'X',
+    20: 'XX',
+    30: 'XXX',
+    40: 'XL',
+    50: 'L',
+    60: 'LX',
+    70: 'LXX',
+    80: 'LXXX',
+    90: 'XC',
+    100: 'C',
+    200: 'CC',
+    300: 'CCC',
+    400: 'CD',
+    500: 'D',
+    600: 'DC',
+    700: 'DCC',
+    800: 'DCCC',
+    900: 'CM',
+    1000: 'M',
+    2000: 'MM',
+    3000: 'MMM',
+};
 
 function intToRoman(num){
     const arr = []; 
     const array = [];
-    const arr0 = [];
     
     for(let i = 0; i<num.toString().length; i++){// each digits push in array
         arr.push(num.toString().charAt(i));
@@ -52,8 +68,16 @@ function intToRoman(num){
 //    console.log(`first digit: ${first}\narray: ${arr}\nlength: ${len}\n`);
     i++
     }
-    
-    console.log(num);
+     
+    const arr0 = [];
+    l = 0;
+    array.forEach(el => {
+        arr0.push(numsi[el])
+    })
+//    console.log(nums[4]);
     console.log(array);
+    console.log(arr0.join(''));
+    return arr0.join('')
+//task: you must write code snippet to each number in array make same as in object nums,so for example 900 it 400, 500 by object and 400 to the same way, then push it into array
 };
 intToRoman(1994);
