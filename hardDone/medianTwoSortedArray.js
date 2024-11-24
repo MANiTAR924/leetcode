2767ba6d647634e2a30314de5815d1bdb38b7b23
manitar <mac21077919050@gmail.com>
2024-11-24 14:39:30 +0200

    save

--- hardDone/medianTwoSortedArray.js
+++ hardDone/medianTwoSortedArray.js
@@ -5,13 +5,9 @@ function median(nums1, nums2){
     nums1.sort((a,b) => {
         return a-b;
     });
-    console.log(nums1[nums1.length / 2 - 1] + nums1[nums1.length / 2]); 
     if (nums1.length % 2 !== 0){
-        console.log(`0the median of [${nums1}] is ${nums1[(nums1.length-1) / 2]}`);
-    console.log('index: ' , (nums1.length-1) / 2)
         return nums1[(nums1.length-1) / 2]
     }else if (nums1.length % 2 === 0){
-        console.log(`1the median of [${nums1}] is ${(nums1[nums1.length / 2 - 1] + nums1[nums1.length / 2]) / 2 }`);
         return (nums1[nums1.length / 2 - 1] + nums1[nums1.length / 2]) / 2;  
     }
 };
